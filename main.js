14bbce48e58532a63ffc68ababe0a53a6e285e44
KAREN C W LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIKA8@OOCL.COM>
2020-07-21 17:59:08 +0800

    realize Multipul table(main.js)

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 function createMultiplyTable(start, end) {
+  if(isNumValid(start, end)){
+    return getMultipulTable(start,end);
+  }
   return null;
+}
+function isNumValid(start,end){
+  if(start >= 1 && start <= 1000){
+    return true;
+  }
+  if(end >= 1 && end <= 1000){
+    return true;
+  }
+  if(start <= end){
+    return true;
+  }
+  return false;
+}
+
+function getMultipulTable(start,end){
+  var mulTable = "";
+  for(var i=start; i<=end; i++){
+    for(var j=start; j<=i; j++){
+      mulTable += j + '*' + i + '=' + j * i + '\t';
+    }
+    mulTable = mulTable.substring(0,mulTable.length-1);
+    if(i != end)
+      mulTable += '\n';
+  }
+  return mulTable;
 }
 
 module.exports = {
